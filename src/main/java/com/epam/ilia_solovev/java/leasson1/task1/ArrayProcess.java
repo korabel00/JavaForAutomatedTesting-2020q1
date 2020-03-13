fb4d3b692caf0e46e1a6144c0fc82e9514fcad90
Ilia Solovev <ilia_solovev@epam.com>
2020-03-13 12:51:52 +0000

    Added some somments

--- src/main/java/com/epam/ilia_solovev/java/leasson1/task1/ArrayProcess.java
+++ src/main/java/com/epam/ilia_solovev/java/leasson1/task1/ArrayProcess.java
@@ -22,24 +22,26 @@ public class ArrayProcess {
         app.startArrayProcess();
     }
 
+    //Running application
     public void startArrayProcess() {
         initializeAnArray();
         findAndSwitchMinPositiveAndMaxNegative();
         outputResult();
     }
 
+    //an Array initialization with random values
     private void initializeAnArray() {
-        //an Array initialization
+
         int maxLimit = 10;
         int minLimit = -10;
 
-        //an Array initialization with random values
         for (int i = 0; i < arrayOfInts.length; i++) {
             arrayOfInts[i] = (int) (Math.random() * ((maxLimit - minLimit) + 1)) + minLimit;
             System.out.print(arrayOfInts[i] + " ");
         }
     }
 
+    //find Min Positive and Max Negative numbers in an Array and its position in the Array;
     private void findAndSwitchMinPositiveAndMaxNegative () {
 
         int minPositiveNumberPlace = 0;
@@ -47,7 +49,6 @@ public class ArrayProcess {
         int minPositiveNumberInArray = 11;
         int maxNegativeNumberInArray = 0;
 
-        //find Min Positive and Max Negative numbers in an Array and its position in the Array;
         for (int i = 0; i < arrayOfInts.length; i++) {
             if (arrayOfInts[i] > 0 && arrayOfInts[i] < minPositiveNumberInArray) {
                 minPositiveNumberInArray = arrayOfInts[i];
@@ -61,7 +62,7 @@ public class ArrayProcess {
         arrayOfInts[minPositiveNumberPlace] = maxNegativeNumberInArray;
         arrayOfInts[maxNegativeNumberPlace] = minPositiveNumberInArray;
     }
-
+    
     private void outputResult() {
 
         System.out.println();
