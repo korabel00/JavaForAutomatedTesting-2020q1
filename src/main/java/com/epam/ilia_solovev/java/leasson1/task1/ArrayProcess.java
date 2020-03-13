e11f98799908c7274448407040ea668557e9aff1
Ilia Solovev <ilia_solovev@epam.com>
2020-03-13 12:49:17 +0000

    Update ArrayProcess.java

--- src/main/java/com/epam/ilia_solovev/java/leasson1/task1/ArrayProcess.java
+++ src/main/java/com/epam/ilia_solovev/java/leasson1/task1/ArrayProcess.java
@@ -1,4 +1,4 @@
-/*Ilia Solovev. Task 1, Option 1
+/**Ilia Solovev. Task 1, Option 1
 
 Написать программу, осуществляющую обработку массива. Исходные
 условия: массив содержит только целые числа от -10 до 10 (допускается
@@ -14,6 +14,8 @@
 package com.epam.ilia_solovev.java.leasson1.task1;
 
 public class ArrayProcess {
+    //Creating the array with length 20
+    private int[] arrayOfInts = new int[20];
 
     public static void main(String[] args) {
         ArrayProcess app = new ArrayProcess();
@@ -21,35 +23,50 @@ public class ArrayProcess {
     }
 
     public void startArrayProcess() {
-        int arraySize = 10;
+        initializeAnArray();
+        findAndSwitchMinPositiveAndMaxNegative();
+        outputResult();
+    }
+
+    private void initializeAnArray() {
+        //an Array initialization
         int maxLimit = 10;
         int minLimit = -10;
-        int[] arrayOfInts = new int[arraySize];
-        int maxNumberInArray = arrayOfInts[0];
-        int minnNumberInArray = arrayOfInts[0];
-        int maxNumberPlace = 0;
-        int minNumberPlace = 0;
 
         //an Array initialization with random values
-        for (int i = 0; i < arraySize; i++) {
+        for (int i = 0; i < arrayOfInts.length; i++) {
             arrayOfInts[i] = (int) (Math.random() * ((maxLimit - minLimit) + 1)) + minLimit;
-            System.out.println(arrayOfInts[i]);
+            System.out.print(arrayOfInts[i] + " ");
         }
-
-        //find Min and Aax numbers in an Array and its position in an Array;
-        for (int i = 1; i < arraySize; i++) {
-            if (arrayOfInts[i] > maxNumberInArray) {
-                maxNumberInArray = arrayOfInts[i];
-                maxNumberPlace = i;
-            }
-            if (arrayOfInts[i] < minnNumberInArray) {
-                minnNumberInArray = arrayOfInts[i];
-                minNumberPlace = i;
-            }
-        }
-
-
-        System.out.println("maxNumber: " + maxNumberInArray + " Position :" + maxNumberPlace);
-        System.out.println("minNumber: " + minnNumberInArray + " Position :" + minNumberPlace);
     }
+
+    private void findAndSwitchMinPositiveAndMaxNegative () {
+
+        int minPositiveNumberPlace = 0;
+        int maxNegativeNumberPlace = 0;
+        int minPositiveNumberInArray = 11;
+        int maxNegativeNumberInArray = 0;
+
+        //find Min Positive and Max Negative numbers in an Array and its position in the Array;
+        for (int i = 0; i < arrayOfInts.length; i++) {
+            if (arrayOfInts[i] > 0 && arrayOfInts[i] < minPositiveNumberInArray) {
+                minPositiveNumberInArray = arrayOfInts[i];
+                minPositiveNumberPlace = i;
+            }
+            if (arrayOfInts[i] < maxNegativeNumberInArray) {
+                maxNegativeNumberInArray = arrayOfInts[i];
+                maxNegativeNumberPlace = i;
+            }
+        }
+        arrayOfInts[minPositiveNumberPlace] = maxNegativeNumberInArray;
+        arrayOfInts[maxNegativeNumberPlace] = minPositiveNumberInArray;
+    }
+
+    private void outputResult() {
+
+        System.out.println();
+        for  (int i = 0; i < arrayOfInts.length; i++) {
+            System.out.print(arrayOfInts[i] + " ");
+            }
+        }
 }
