e8570cefde07e8a17b3d62abd27467a1ccff26cb
Ilia Solovev <ilia_solovev@epam.com>
2020-03-16 17:49:40 +0000

    Add new file

---
+++ src/main/java/com/epam/ilia_solovev/java/lesson2/task1/SortArray.java
@@ -0,0 +1,25 @@
+package com.epam.ilia_solovev.java.lesson2.task1;
+
+public class SortArray {
+
+    public static void bubbleSortByPowerConsumption (HomeAppliances[] array) {
+
+        boolean needIteration = true;
+
+        while (needIteration) {
+            needIteration = false;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i].getPowerConsumption() < array[i - 1].getPowerConsumption()) {
+                    swap(array, i, i - 1);
+                    needIteration = true;
+                }
+            }
+        }
+    }
+
+    private static void swap (HomeAppliances[] array, int inddexOne, int indexTwo) {
+        HomeAppliances temp = array[inddexOne];
+        array[inddexOne] = array[indexTwo];
+        array[indexTwo] = temp;
+    }
+}
