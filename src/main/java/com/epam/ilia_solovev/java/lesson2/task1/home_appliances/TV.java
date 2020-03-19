5f94b3c09f33be66bd1f90810f80ca2adf44e9b1
Ilia Solovev <ilia_solovev@epam.com>
2020-03-19 17:03:13 +0400

    Added lesson 2 task with fixes

--- src/main/java/com/epam/ilia_solovev/java/lesson2/task1/home_appliances/TV.java
+++ src/main/java/com/epam/ilia_solovev/java/lesson2/task1/home_appliances/TV.java
@@ -17,7 +17,7 @@ public class TV extends HomeAppliances implements Connectible {
             System.out.println("I'am a TV - Showing you Game of Thrones. It is so nice to watch it on a " + screenSize +
                     " inches screen, isn't?");
         } else {
-            System.out.println("First you need to turn on me");
+            System.out.println("First, you need to turn me on");
         }
     }
 
